444ea6138427df1329cf205e365c22735a1b07a3
Amanda Alcantara <amanda.alcantara06@gmail.com>
2023-09-25 00:29:08 -0300

    Segundo commit

---
+++ src/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = () => {
+    if (email === 'eduardo.lino@pucpr.br' && password === '123456') {
+      setMessage('Acessado com sucesso!');
+    } else {
+      setMessage('Usuário ou senha incorretos!');
+    }
+  };
+
+  return (
+    <center><div className="container">
+      <h1>Login</h1>
+      <div>
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Senha:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={handleLogin}>Acessar</button>
+      <p>{message}</p>
+    </div></center>
+  );
+}
+
+export default Login;
